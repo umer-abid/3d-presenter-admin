8109ace6255063f32af7bd0194591b6178e30522
umer123456 <umerranavvork@gmail.com>
2021-06-08 17:53:19 +0500

    Added Data Table dynamically

---
+++ js/pages Js/editDeleteRoom.js
@@ -0,0 +1,8 @@
+function edit_row(no) {
+  document.getElementById("edit_button" + no) = "test";
+  
+}
+function delete_row(no)
+{
+ document.getElementById("delete_button"+no).outerHTML="test";
+}
--- js/pages Js/roomTable.js
+++ js/pages Js/roomTable.js
@@ -13,7 +13,9 @@ $(document).ready(function () {
     <td>12/01/2001</td>
     <td class="text-center">
       <a href="./editRoom.html">
-        <button class="btn btn-success" value = '` +
+        <button id="edit_button` +
+        ids[i] +
+        `" class="btn btn-success" value = '` +
         ids[i] +
         `'>
           Edit<i class="ml-1 far fa-edit"></i>
@@ -21,7 +23,9 @@ $(document).ready(function () {
       </a>
       <button value=` +
         ids[i] +
-        ` type="button" class="btn btn-danger mt-1 mt-md-0 mt-xl-0" data-toggle="modal" data-target="#deleteModal">
+        ` type="button" id="delete_button` +
+        ids[i] +
+        `" class="btn btn-danger mt-1 mt-md-0 mt-xl-0" data-toggle="modal" data-target="#deleteModal">
         Delete<i class="ml-1 fas fa-trash-alt"></i>
       </button>
     </td>
